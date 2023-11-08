fecbe9e92cfc4969236b0421518c70c131b7bf72
manjirigole <119617326+manjirigole@users.noreply.github.com>
2023-11-08 18:56:38 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,8 @@
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+  });
+  
+  const User = mongoose.model('User', userSchema);
+  module.exports = UserModel;
