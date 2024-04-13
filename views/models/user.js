5293bc4d963a10b9e79184e4e5d6fff55f43e184
Manjiri Gole <119617326+manjirigole@users.noreply.github.com>
2024-04-13 14:11:33 +0530

    Delete views directory

--- views/models/user.js
+++
@@ -1,8 +0,0 @@
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-  });
-  
-  const User = mongoose.model('User', userSchema);
-  module.exports = UserModel;
